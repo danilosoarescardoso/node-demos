cfd74007931554ab14bb5ddb30c01aa050d20151
Danilo Cardoso <dsc.soares@gmail.com>
2019-11-25 19:23:16 -0300

    trabalhando com classes, promisify e leitura de arquivos

---
+++ 06-cli/test.js
@@ -0,0 +1,16 @@
+const { deepEqual, ok } = require("assert")
+const database = require('./database')
+
+const DEFAULT_ITEM_CADASTRAR = {
+  nome: "Flash",
+  poder: "Speed",
+  id: 1
+}
+
+describe("Suite de manipulacao de Herois", () => {
+  it("deve pesquisar um heroi usando arquivos", async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR
+    const resultado = await database.listar(expected.id)
+    ok(resultado, expected)
+  })
+})
